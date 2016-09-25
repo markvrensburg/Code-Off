b8a915f7c29b6b9315ffe2d60ee342a6c78c2a68
Mark van Rensburg <jansevanrensburg.mark@gmail.com>
2016-09-25 21:38:37 +0200

    Immutable search implementation

--- codeOff/shared/src/main/scala/codeoff/core/search/AStarImmutable.scala
+++ codeOff/shared/src/main/scala/codeoff/core/search/AStarImmutable.scala
@@ -1,6 +1,7 @@
 package codeoff.core.search
 
 import scala.annotation.tailrec
+import scala.collection.immutable.{HashMap, HashSet}
 import scalaz.{Heap, TreeLoc}
 
 object AStarImmutable {
@@ -11,51 +12,31 @@ object AStarImmutable {
       from.path.map(_.action).reverse.tail.toList
 
     def successors[AA](node: TreeLoc[AA]): List[TreeLoc[AA]] = {
-      def go(current: Option[TreeLoc[AA]], accum: List[TreeLoc[AA]]): List[TreeLoc[AA]] =
-        current.fold(accum)(n => go(n.right, accum :+ n))
+      @tailrec
+      def go(current: Option[TreeLoc[AA]], accum: List[TreeLoc[AA]]): List[TreeLoc[AA]] = current match {
+        case Some(l) => go(l.right, accum :+ l)
+        case None => accum
+      }
       go(node.firstChild, List.empty)
     }
 
     @tailrec
-    def go(open: Heap[TreeLoc[InformedSearchNode[A, B]]], closed: Vector[TreeLoc[InformedSearchNode[A, B]]]): List[B] =
+    def go(open: Heap[TreeLoc[InformedSearchNode[A, B]]], closed: Set[A], cache: Map[A,Double]): List[B] =
       open.uncons match {
         case Some((current, remainder)) =>
           if (goal(current.getLabel.state))
             retrievePlan(current)
           else {
-            successors(current).filterNot(closed.exists)
-
-
-            go(remainder.insertAll(successors(current)), closed :+ current)
+            val candidate = successors(current).filterNot(x =>
+              closed.contains(x.getLabel.state) ||
+              cache.get(x.getLabel.state).fold(false)(_ > x.getLabel.g))
+            go(remainder.insertAll(candidate),
+              closed + current.getLabel.state,
+              cache ++ candidate.map(x => x.getLabel.state -> x.getLabel.g))
           }
         case None => List.empty
       }
 
-    /*
-     if (open.nonEmpty) {
-       val current = selectNode(open)
-       if (goal(current.getLabel.state))
-         retrievePlan(current)
-       else {
-         val (openMod, closedMod) = successors(current).foldLeft((open.filter(_ != current), closed :+ current)) { (a, n) => {
-           val state = n.getLabel.state
-           val openAcc = a._1
-           val closedAcc = a._2
-           if (closedAcc.exists(_.getLabel.state == state))
-             (openAcc, closedAcc)
-           else if (openAcc.exists(_.getLabel.state == state)) {
-             (openAcc.filterNot(x => (x.getLabel.state == state) && (x.getLabel.g > n.getLabel.g)), closedAcc)
-           }
-           else
-             (openAcc :+ n, closedAcc)
-         }}
-         go(openMod, closedMod)
-       }
-     } else
-       List.empty
-   }                  */
-
-
-    go(Heap.singleton(tree.tree.loc), Vector.empty)
+    go(Heap.singleton(tree.tree.loc), HashSet.empty, HashMap(tree.tree.rootLabel.state -> tree.tree.rootLabel.g))
   }
 }
