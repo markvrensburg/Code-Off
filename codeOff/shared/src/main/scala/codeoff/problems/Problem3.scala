ec835862c764e74af48091890b256ea6ffb93d5e
Mark J v Rensburg <markjvr@net1.com>
2016-10-05 16:40:56 +0200

    Problem 3 solution updated to include same sized water regions

--- codeOff/shared/src/main/scala/codeoff/problems/Problem3.scala
+++ codeOff/shared/src/main/scala/codeoff/problems/Problem3.scala
@@ -1,6 +1,7 @@
 package codeoff.problems
 
 import codeoff.fileio.FileIO
+import codeoff.model.Location
 import codeoff.parser.WaterSourceParser
 
 object Problem3 {
@@ -11,7 +12,11 @@ object Problem3 {
     inputFiles.foreach{file =>
       val text = io.readFileLines(s"$directory/$file")
       val state = WaterSourceParser.parse(text.mkString).get
-      val solution= state.mark(state.waterRegions.maxBy(_.size))
+      val solution= state.mark{
+        val regions = state.waterRegions
+        val largestSize = regions.maxBy(_.size).size
+        regions.filter(_.size == largestSize).foldLeft(Set.empty[Location])(_ ++ _)
+      }
       val solutionOutput = solution.draw
       io.writeFileLines(s"$directory/${file.replace(".in", ".out")}", solutionOutput)
     }
